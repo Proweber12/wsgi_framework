633598b69c051c1a64e633b162acd830fcff5e76
Mihail <borisonchick@gmail.com>
2022-09-20 18:41:55 +0300

    Returned HTTP-requests

--- wsgi_framework/main.py
+++ wsgi_framework/main.py
@@ -1,11 +1,8 @@
-<<<<<<< Updated upstream
-=======
 import html
 from quopri import decodestring
 from wsgi_framework.http_requests import GetRequests, PostRequests
 
 
->>>>>>> Stashed changes
 class PageNotFound404:
     def __call__(self, request):
         return '404 WHAT', '404 PAGE Not Found'
@@ -27,14 +24,27 @@ class Framework:
             view = self.routes_lst[path]
         else:
             view = PageNotFound404()
+
         request = {}
+
+        method = environ['REQUEST_METHOD']
+        request['method'] = method
+
+        if method == 'POST':
+            data = PostRequests().get_request_params(environ)
+            request['data'] = Framework.decode_value(data)
+            print(f'Нам пришёл post-запрос: {Framework.decode_value(data)}')
+        if method == 'GET':
+            request_params = GetRequests().get_request_params(environ)
+            request['request_params'] = Framework.decode_value(request_params)
+            print(f'Нам пришли GET-параметры:'
+                  f' {Framework.decode_value(request_params)}')
+
         for front in self.fronts_lst:
             front(request)
         code, body = view(request)
         start_response(code, [('Content-Type', 'text/html')])
         return [body.encode('utf-8')]
-<<<<<<< Updated upstream
-=======
 
     @staticmethod
     def decode_value(data):
@@ -44,4 +54,3 @@ class Framework:
             val_decode_str = decodestring(val).decode('UTF-8')
             new_data[k] = html.unescape(val_decode_str)
         return new_data
->>>>>>> Stashed changes
